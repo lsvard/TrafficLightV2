0ca94072b9fb2ae17c794c9a04e2d82d670fda31
lsvard <l9244492@bk.ru>
2023-08-09 15:52:13 +0300

    Initial Commit

---
+++ TrafficLightV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightV2
+//
+//  Created by l.s.vard on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
